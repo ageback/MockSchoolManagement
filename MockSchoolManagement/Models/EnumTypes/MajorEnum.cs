2e44f3d5dbc9d581d5c7baab52b79172bbe716a8
Michael Lu <yeskky@gmail.com>
2021-02-07 09:17:54 +0800

    验证错误红色显示；select验证

--- MockSchoolManagement/Models/EnumTypes/MajorEnum.cs
+++ MockSchoolManagement/Models/EnumTypes/MajorEnum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,13 @@ namespace MockSchoolManagement.Models.EnumTypes
 {
     public enum MajorEnum
     {
-        无,
-        计算机科学,
-        物流,
-        电子商务
+        [Display(Name ="未分配")]
+        None,
+        [Display(Name = "计算机科学")]
+        ComputerScience,
+        [Display(Name = "电子商务")]
+        ElectronicCommerce,
+        [Display(Name = "数学")]
+        Mathematics
     }
 }
--- MockSchoolManagement/Models/Student.cs
+++ MockSchoolManagement/Models/Student.cs
@@ -6,12 +6,17 @@ namespace MockSchoolManagement.Models
     public class Student
     {
         public int Id { get; set; }
-        [Display(Name ="姓名")]
-        [Required(ErrorMessage ="请输入姓名，它不能为空！")]
+
+        [Display(Name = "姓名")]
+        [Required(ErrorMessage = "请输入姓名，它不能为空！")]
         public string Name { get; set; }
+
         [Display(Name = "主修科目")]
-        public MajorEnum Major { get; set; }
+        [Required(ErrorMessage ="请选择一门科目")]
+        public MajorEnum? Major { get; set; }
+
         [Display(Name = "电子邮箱")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "邮箱的格式不正确")]
         [Required(ErrorMessage = "请输入邮箱地址，它不能为空！")]
         public string Email { get; set; }
     }
