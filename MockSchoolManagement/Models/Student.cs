5d4d56a4c068a41008de6dc019a9bf9c63c181dc
Michael Lu <yeskky@gmail.com>
2021-02-07 08:49:42 +0800

    自定义显示属性

--- MockSchoolManagement/Models/Student.cs
+++ MockSchoolManagement/Models/Student.cs
@@ -6,9 +6,13 @@ namespace MockSchoolManagement.Models
     public class Student
     {
         public int Id { get; set; }
+        [Display(Name ="姓名")]
         [Required(ErrorMessage ="请输入姓名，它不能为空！")]
         public string Name { get; set; }
-        public MajorEnum Major { get; set; }        
+        [Display(Name = "主修科目")]
+        public MajorEnum Major { get; set; }
+        [Display(Name = "电子邮箱")]
+        [Required(ErrorMessage = "请输入邮箱地址，它不能为空！")]
         public string Email { get; set; }
     }
 }
