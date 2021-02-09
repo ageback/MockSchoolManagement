a22add7b2c4f807340540e20df08f00e95d1b2b0
Michael Lu <yeskky@gmail.com>
2021-02-09 16:20:12 +0800

    注入ILogger服务

--- MockSchoolManagement/Controllers/ErrorController.cs
+++ MockSchoolManagement/Controllers/ErrorController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,9 @@ namespace MockSchoolManagement.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> logger;
+
+        public ErrorController(ILogger<ErrorController> logger) => this.logger = logger;
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
@@ -17,9 +21,10 @@ namespace MockSchoolManagement.Controllers
             switch (statusCode)
             {
                 case 404:
-                    ViewBag.ErrorMessage = "抱歉，用户访问的页面不存在";
-                    ViewBag.Path = statusCodeResult.OriginalPath;
-                    ViewBag.QS = statusCodeResult.OriginalQueryString;
+                    logger.LogWarning($"发生了一个404错误。路径 = {statusCodeResult.OriginalPath} 以及查询字符串 = {statusCodeResult.OriginalQueryString}");
+                    //ViewBag.ErrorMessage = "抱歉，用户访问的页面不存在";
+                    //ViewBag.Path = statusCodeResult.OriginalPath;
+                    //ViewBag.QS = statusCodeResult.OriginalQueryString;
                     break;
             }
             return View("NotFound");
@@ -29,9 +34,10 @@ namespace MockSchoolManagement.Controllers
         public IActionResult Error()
         {
             var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-            ViewBag.ExceptionPath = exceptionHandlerPathFeature.Path;
-            ViewBag.ExceptionMessage = exceptionHandlerPathFeature.Error.Message;
-            ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
+            logger.LogError($"路径{exceptionHandlerPathFeature.Path} 产生了一个错误{exceptionHandlerPathFeature.Error}");
+            //ViewBag.ExceptionPath = exceptionHandlerPathFeature.Path;
+            //ViewBag.ExceptionMessage = exceptionHandlerPathFeature.Error.Message;
+            //ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
             return View("Error");
     }
     }
