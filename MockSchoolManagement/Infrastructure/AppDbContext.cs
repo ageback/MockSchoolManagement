8b8074bb95bbe65d61485de5282cb07c9f6bb5ab
Michael Lu <yeskky@gmail.com>
2021-02-12 09:48:26 +0800

    添加数据库字段 Add-Migration Extend_IdentityUser;Update-Database

--- MockSchoolManagement/Infrastructure/AppDbContext.cs
+++ MockSchoolManagement/Infrastructure/AppDbContext.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace MockSchoolManagement.Infrastructure
 {
-    public class AppDbContext : IdentityDbContext
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
