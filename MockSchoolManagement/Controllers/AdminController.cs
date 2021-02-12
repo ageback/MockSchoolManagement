875ea431a53e61aa453641af786047cd72ea39a5
Michael Lu <yeskky@gmail.com>
2021-02-12 10:12:23 +0800

    创建角色功能

---
+++ MockSchoolManagement/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MockSchoolManagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockSchoolManagement.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(RoleManager<IdentityRole> roleManager) => _roleManager = roleManager;
+
+        [HttpGet]
+        public IActionResult CreateRole() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole { Name = model.RoleName };
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                    return RedirectToAction("index", "home");
+
+                foreach (IdentityError err in result.Errors)
+                    ModelState.AddModelError("", err.Description);
+            }
+
+            return View(model);
+        }
+    }
+}
