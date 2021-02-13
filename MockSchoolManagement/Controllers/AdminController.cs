5d8435d419165a8108d47ccfaeb5ffa879ddf966
Michael Lu <yeskky@gmail.com>
2021-02-13 10:21:42 +0800

    编辑角色功能

--- MockSchoolManagement/Controllers/AdminController.cs
+++ MockSchoolManagement/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MockSchoolManagement.Models;
 using MockSchoolManagement.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -11,8 +12,13 @@ namespace MockSchoolManagement.Controllers
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AdminController(RoleManager<IdentityRole> roleManager) => _roleManager = roleManager;
+        public AdminController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager; 
+            _userManager = userManager; 
+        }
 
         [HttpGet]
         public IActionResult CreateRole() => View();
@@ -36,5 +42,59 @@ namespace MockSchoolManagement.Controllers
 
         [HttpGet]
         public IActionResult ListRoles() => View(_roleManager.Roles);
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"角色 Id={id} 的信息不存在，请重试。";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            var users = _userManager.Users.ToList();
+            foreach (var user in users)
+            {
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"角色 Id={model.Id} 的信息不存在，请重试。";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var result = await _roleManager.UpdateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+                foreach(var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+                return View(model);
+            }
+        }
     }
 }
