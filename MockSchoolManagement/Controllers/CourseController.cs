efd1e424ec6286c8b9e8fb1797f0447cba0b4ea5
Michael Lu <yeskky@gmail.com>
2021-02-17 14:59:18 +0800

    创建CourseController并注入ICourseRepository

---
+++ MockSchoolManagement/Controllers/CourseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MockSchoolManagement.DataRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockSchoolManagement.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CourseController(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+        public ActionResult Index() => View();
+    }
+}
---
+++ MockSchoolManagement/DataRepositories/SQLCourseRepository.cs
@@ -0,0 +1,13 @@
+﻿using MockSchoolManagement.Infrastructure;
+using MockSchoolManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MockSchoolManagement.DataRepositories
+{
+    public class SQLCourseRepository
+    {
+    }
+}
